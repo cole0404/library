b5d5a639c49bb8fb13c759f5d9c525e72cb368f6
cole0404 <coletonboles2004@gmail.com>
2023-01-28 13:52:16 -0600

    Visually works fine. Array items not being removed

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let myLibrary = [];
 
 //Selectors here:
+const main = document.querySelector("main");
+const form = document.querySelector("form");
 const bookButton = document.querySelector("#bookButton");
 const submitButton = document.querySelector(".popup button");
 
@@ -9,26 +11,74 @@ bookButton.addEventListener("click", openForm);
 submitButton.addEventListener("click", addBookToLibrary);
 
 //book constructor
-function book(title, author, pages, read) {
+function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
 }
 
-//why did moving selectors from global to inside function work?
 function addBookToLibrary() {
   const title = document.querySelector("#title").value;
   const author = document.querySelector("#author").value;
   const pages = document.querySelector("#pages").value;
-  const read = document.querySelector("#read").value;
+  const read = document.querySelector("#read").checked;
 
-  let newBook = new book(title, author, pages, read);
+  let newBook = new Book(title, author, pages, read);
 
-  myLibrary.push(newBook);
-  console.log(myLibrary);
-  document.forms["popup"].reset();
-  document.getElementById("popup").style.display = "none";
+  if (form.checkValidity() == false) return;
+  else {
+    myLibrary.push(newBook);
+    document.forms["popup"].reset();
+    document.getElementById("popup").style.display = "none";
+
+    const container = document.createElement("div");
+    const titleDiv = document.createElement("div");
+    const authorDiv = document.createElement("div");
+    const pagesDiv = document.createElement("div");
+    const readButton = document.createElement("button");
+    const removeButton = document.createElement("button");
+
+    container.className = "container";
+    titleDiv.className = "titleDiv";
+    authorDiv.className = "authorDiv";
+    pagesDiv.className = "pagesDiv";
+    readButton.className = "readButton";
+    removeButton.id = "removeButton";
+
+    titleDiv.textContent = `"${newBook.title}"`;
+    authorDiv.textContent = `${newBook.author}`;
+    pagesDiv.textContent = `Pages: ${newBook.pages}`;
+    if (newBook.read == true) {
+      readButton.textContent = `Read`;
+    } else readButton.textContent = `Not read`;
+    removeButton.textContent = `Remove`;
+
+    readButton.addEventListener("click", function () {
+      if (newBook.read == true) {
+        newBook.read = false;
+        readButton.textContent = "Not read";
+      } else {
+        newBook.read = true;
+        readButton.textContent = "Read";
+      }
+    });
+
+    removeButton.addEventListener("click", function () {
+      main.removeChild(container);
+    });
+
+    main.appendChild(container);
+    container.appendChild(titleDiv);
+    container.appendChild(authorDiv);
+    container.appendChild(pagesDiv);
+    container.appendChild(readButton);
+    container.appendChild(removeButton);
+
+    form.addEventListener("submit", function (event) {
+      event.preventDefault();
+    });
+  }
 }
 
 function openForm() {
