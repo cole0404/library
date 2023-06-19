4d8a093b5b098e49892b5c80ace2950c49fcb965
cole0404 <coletonboles2004@gmail.com>
2023-06-19 18:12:11 -0500

    crisian help

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let myLibrary = [];
+let newBook;
 
 //Selectors:
 const main = document.querySelector("main");
@@ -27,50 +28,62 @@ function openForm() {
 function addBookToLibrary() {
   if (form.checkValidity() == false) return;
   else {
-    globalThis.newBook = new Book();
+    newBook = new Book();
     myLibrary.push(newBook);
+
     document.forms["popup"].reset();
     document.getElementById("popup").style.display = "none";
 
-    const card = document.createElement("div");
-    const titleDiv = document.createElement("div");
-    const authorDiv = document.createElement("div");
-    const pagesDiv = document.createElement("div");
-    const readButton = document.createElement("button");
-    const removeButton = document.createElement("button");
+    createCard();
+  }
+}
 
-    card.className = "card";
-    titleDiv.className = "titleDiv";
-    authorDiv.className = "authorDiv";
-    pagesDiv.className = "pagesDiv";
-    readButton.className = "readButton";
-    removeButton.id = "removeButton";
+function createCard() {
+  const card = document.createElement("div");
+  const titleDiv = document.createElement("div");
+  const authorDiv = document.createElement("div");
+  const pagesDiv = document.createElement("div");
+  const readButton = document.createElement("button");
+  const removeButton = document.createElement("button");
 
-    titleDiv.textContent = `"${newBook.title}"`;
-    authorDiv.textContent = `${newBook.author}`;
-    pagesDiv.textContent = `Pages: ${newBook.pages}`;
-    removeButton.textContent = `Remove`;
-    if (newBook.read == true) {
-      readButton.textContent = `Read`;
-      readButton.style.background = "#ef5a5a";
-    } else {
-      readButton.textContent = `Not read`;
-      readButton.style.background = "#ffa952";
+  card.className = "card";
+  titleDiv.className = "titleDiv";
+  authorDiv.className = "authorDiv";
+  pagesDiv.className = "pagesDiv";
+  readButton.className = "readButton";
+  removeButton.className = "removeButton";
+
+  titleDiv.textContent = `"${newBook.title}"`;
+  authorDiv.textContent = `${newBook.author}`;
+  pagesDiv.textContent = `Pages: ${newBook.pages}`;
+  removeButton.textContent = `Remove`;
+  if (newBook.read == true) {
+    readButton.textContent = `Read`;
+    readButton.style.background = "#ef5a5a";
+  } else {
+    readButton.textContent = `Not read`;
+    readButton.style.background = "#ffa952";
+  }
+
+  readButton.addEventListener("click", newBook.toggleRead);
+
+  //when cards are created, create a index of each new card that will correspond with library array
+  removeButton.addEventListener("click", function () {
+    let cards = document.querySelectorAll(".removeButton");
+
+    for (let i = 0; i < cards; i++) {
+      if (i == ) myLibrary.splice(i, 1);
     }
 
-    readButton.addEventListener("click", newBook.toggleRead);
+    main.removeChild(card);
+  });
 
-    removeButton.addEventListener("click", function () {
-      main.removeChild(card);
-    });
-
-    main.appendChild(card);
-    card.appendChild(titleDiv);
-    card.appendChild(authorDiv);
-    card.appendChild(pagesDiv);
-    card.appendChild(readButton);
-    card.appendChild(removeButton);
-  }
+  main.appendChild(card);
+  card.appendChild(titleDiv);
+  card.appendChild(authorDiv);
+  card.appendChild(pagesDiv);
+  card.appendChild(readButton);
+  card.appendChild(removeButton);
 }
 
 Book.prototype.toggleRead = function () {
